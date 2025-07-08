d1bbf9d35397f80d150fc1cf4331ad02775b4de2
atul030702 <singhatul0307@gmail.com>
2025-07-08 22:02:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header.jsx";
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Header />
+      Atul Kumar
+    </div>
+  );
+}
+
+export default App;
